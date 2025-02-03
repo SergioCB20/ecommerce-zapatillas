86ec3a08ce9bdab9d37389060778c554215780d3
Sergio <sergiochumbi98@gmail.com>
2025-02-03 15:16:57 -0500

    pequeña modificacion

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -1,6 +1,6 @@
 
 export default function ProductsPage(){
     return (
-        <h1>Página de productos (buscador, filtros)</h1>
+        <h1 className="text-red-500">Página de productos (buscador, filtros)</h1>
     )
 }
