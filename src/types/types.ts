9215f8129c0fa873ee7c6f3801566ee22ecfeb6a
Sergio <sergiochumbi98@gmail.com>
2025-02-03 15:34:09 -0500

    pequeños cambios 2.0

---
+++ src/types/types.ts
@@ -0,0 +1,12 @@
+
+export interface Product {
+    id:string,
+    name:string,
+    price:number,
+    brand:string,
+    category:string,
+    imageUrl:string,
+    hasDiscount:boolean,
+    discountPorcentage?:number,
+    stock:number
+}
